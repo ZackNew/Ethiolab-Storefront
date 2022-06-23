216e9267058b3c892bc570f5ebe8f0c1850b2636
selight <ghiwotselam@gmail.com>
2022-06-24 01:33:33 +0300

    cms updated

--- packages/api-client/src/api/cms/cmsQuery.ts
+++ packages/api-client/src/api/cms/cmsQuery.ts
@@ -1,6 +1,6 @@
 import gql from 'graphql-tag';
 export default gql`
-    query getCms($type:Type!) {
+    query getCms($type:[Type!]!) {
         getCms(type:$type) {
             id
             content
