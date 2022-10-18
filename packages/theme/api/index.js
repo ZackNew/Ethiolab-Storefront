71443f30678cca1bfe896189df62d246f00e8c07
Kirubel <kirubelgirmay21@gmail.com>
2022-10-18 16:41:39 +0300

    api corrected

---
+++ packages/theme/api/index.js
@@ -0,0 +1,33 @@
+// api/index.js
+
+const express = require('express')
+const axios = require("axios");
+
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/test',  async function (req, res) {
+  console.log("request value is ", req.body);
+  const body = req.body;
+
+   
+
+  try{
+      await axios.post("http://196.188.120.3:10443/service-openup/toTradeWebPay",body).then((resp) => {
+              console.log("response value is ", resp.data)
+              res.status(200).json({data: resp.data});
+          })
+
+  }catch(err){ 
+      console.log("the error is ", err)
+      res.send("failed")
+  }
+
+})
+
+
+export default {
+  path: '/api',
+  handler: app
+}
