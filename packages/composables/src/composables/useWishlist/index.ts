1093d4e0acbe163015762138d7119fa6e932f3b8
zackX <zekariyasmulugeta91@gmail.com>
2022-12-15 09:29:25 +0300

    wishlist bug fixed (deleted products will not come, the x button deletes only one and recalculate the list, loads again when adding to list)

--- packages/composables/src/composables/useWishlist/index.ts
+++ packages/composables/src/composables/useWishlist/index.ts
@@ -2,13 +2,17 @@
 import {
   Context,
   useWishlistFactory,
-  UseWishlistFactoryParams
+  UseWishlistFactoryParams,
 } from '@vue-storefront/core';
 import type { Wishlist, WishlistItem } from '@vue-storefront/vendure-api';
 import type { AgnosticProductVariant } from '../../types';
 import { getWishlist, setWishlist } from '../../helpers';
 
-const params: UseWishlistFactoryParams<Wishlist, WishlistItem, AgnosticProductVariant> = {
+const params: UseWishlistFactoryParams<
+  Wishlist,
+  WishlistItem,
+  AgnosticProductVariant
+> = {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   load: async (context: Context) => {
     if (process.client) {
@@ -27,7 +31,9 @@ const params: UseWishlistFactoryParams<Wishlist, WishlistItem, AgnosticProductVa
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   removeItem: async (context: Context, { product }) => {
     const wishlist = getWishlist();
-    const updatedWishlist = wishlist.filter(wishlistItem => wishlistItem.sku !== product.sku);
+    const updatedWishlist = wishlist.filter(
+      (wishlistItem) => wishlistItem._variantId !== product._variantId
+    );
     setWishlist(updatedWishlist);
     return updatedWishlist;
   },
@@ -43,14 +49,19 @@ const params: UseWishlistFactoryParams<Wishlist, WishlistItem, AgnosticProductVa
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   isInWishlist: (context: Context, { product }) => {
     const wishlist = process.client ? getWishlist() : [];
-    return Boolean(wishlist?.find((wishlistItem): boolean => {
-
-      const equalSku = wishlistItem?.sku === product?.sku;
-      const equalId = wishlistItem?._id === product?._id;
-      const equalVariantId = wishlistItem?._variantId === product?._variantId;
-      return Boolean(equalSku && equalId && equalVariantId);
-    }));
-  }
+    return Boolean(
+      wishlist?.find((wishlistItem): boolean => {
+        const equalSku = wishlistItem?.sku === product?.sku;
+        const equalId = wishlistItem?._id === product?._id;
+        const equalVariantId = wishlistItem?._variantId === product?._variantId;
+        return Boolean(equalSku && equalId && equalVariantId);
+      })
+    );
+  },
 };
 
-export const useWishlist = useWishlistFactory<Wishlist, WishlistItem, AgnosticProductVariant>(params);
+export const useWishlist = useWishlistFactory<
+  Wishlist,
+  WishlistItem,
+  AgnosticProductVariant
+>(params);
