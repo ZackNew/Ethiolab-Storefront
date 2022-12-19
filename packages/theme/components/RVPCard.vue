cc54105cd659aaab0e3e8e1e34c66ab60da94d05
Kirubel <kirubelgirmay21@gmail.com>
2022-12-19 10:14:08 +0300

    rvp title

--- packages/theme/components/RVPCard.vue
+++ packages/theme/components/RVPCard.vue
@@ -187,7 +187,7 @@
         data-testid="product-link"
         v-on="$listeners"
       >
-        <span class="ssf-product-card__title min-h-[5rem] text-[0.75rem] overflow-hidden">
+        <span class="ssf-product-card__title min-h-[5rem] text-[0.5rem] overflow-hidden">
           {{ title }}
         </span>
       </SfButton> -->
