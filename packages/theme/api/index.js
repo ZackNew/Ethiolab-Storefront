730c7af96a7684ad83dad83d77528c865b34dc8e
Gebriel Dejene <gebrieldejene2@gmail.com>
2023-08-31 11:47:59 +0300

    images fixed

--- packages/theme/api/index.js
+++ packages/theme/api/index.js
@@ -56,7 +56,7 @@ app.post('/shop', async function (req, res) {
 
     try {
       await axios
-        .post('http://10.10.20.45:3000/shop-api', body, options)
+        .post('https://196.188.235.251/shop-api', body, options)
         .then((resp) => {
           res.status(200).json({ data: resp.data });
         })
