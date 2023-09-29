7ce7bc7e43bd8ee575d165a9ffdb35b0f1bb97f2
Gebriel Dejene <gebrieldejene2@gmail.com>
2023-09-29 15:51:47 +0300

    friday updates

--- packages/theme/helpers/category.ts
+++ packages/theme/helpers/category.ts
@@ -1,3 +1,5 @@
 import { AgnosticCategoryTree } from '@vue-storefront/core';
 
-export const getTreeWithoutEmptyCategories = (categoryTree: AgnosticCategoryTree[]): AgnosticCategoryTree[] => categoryTree?.filter(tree => tree.items.length);
+export const getTreeWithoutEmptyCategories = (
+  categoryTree: AgnosticCategoryTree[]
+): AgnosticCategoryTree[] => categoryTree?.filter((tree) => tree.items.length);
--- packages/theme/helpers/product.ts
+++ packages/theme/helpers/product.ts
@@ -1,15 +1,22 @@
 import { Product, ProductVariant } from '@vue-storefront/vendure-api';
 
 // TODO: Refactor logic and probably move to getter
-export const getProductVariantByConfiguration = (product: Product, configuration: unknown): ProductVariant => {
+export const getProductVariantByConfiguration = (
+  product: Product,
+  configuration: unknown
+): ProductVariant => {
   if (!product || !configuration) return;
 
-  const productVariants = product?.variants?.filter(variant => {
+  const productVariants = product?.variants?.filter((variant) => {
     const configurationValues = Object.values(configuration);
 
-    const configurationOptions = variant?.options?.map(option => configurationValues?.filter(value => value === option?.code));
+    const configurationOptions = variant?.options?.map((option) =>
+      configurationValues?.filter((value) => value === option?.code)
+    );
 
-    const isConfigurationLengthEqualToOptions = configurationOptions.flat().length === variant?.options.length && configurationOptions.flat().length === configurationValues.length;
+    const isConfigurationLengthEqualToOptions =
+      configurationOptions.flat().length === variant?.options.length &&
+      configurationOptions.flat().length === configurationValues.length;
 
     return isConfigurationLengthEqualToOptions ? variant : null;
   });
