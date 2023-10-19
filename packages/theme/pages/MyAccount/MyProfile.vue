7f7d6d1feefd9f26175d4808719829fef9ca01ca
tomi-kid <tomiketema209@gmail.com>
2023-10-19 10:51:59 +0300

    Remove mistake

--- packages/theme/pages/MyAccount/MyProfile.vue
+++ packages/theme/pages/MyAccount/MyProfile.vue
@@ -225,7 +225,7 @@ export default {
       let name = cname + '=';
       let decodedCookie = decodeURIComponent(document.cookie);
       let ca = decodedCookie.split(';');
-      for (let i = 0; i < ca.length; i++) {x  
+      for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
         while (c.charAt(0) == ' ') {
           c = c.substring(1);
