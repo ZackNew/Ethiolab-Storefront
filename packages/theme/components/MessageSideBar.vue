3ccc76bc1b69e3a4fce7edd8248965d0dcd3f3d6
tomi-kid <tomiketema209@gmail.com>
2023-11-03 17:02:01 +0300

    ChatUpdate_By_The_Comment

--- packages/theme/components/MessageSideBar.vue
+++ packages/theme/components/MessageSideBar.vue
@@ -1,7 +1,8 @@
 <template>
   <div
-    class="chat rounded-xl shadow-lg bg-white max-h-96 overflow-auto no-scrollbar" ref="chatContainer"
-    >
+    class="chat rounded-xl shadow-lg bg-white max-h-96 overflow-auto no-scrollbar"
+    ref="chatContainer"
+  >
     <div
       class="invisible lg:visible h-20 w-[20rem] bg-secondary text-white rounded-t flex items-center fixed z-50"
     >
@@ -14,23 +15,34 @@
       />
       <h3 class="font-bold text-[20px] whitespace-nowrap">Ethiolab Support</h3>
     </div>
-    <div v-if="!isAuthenticated && !isFormOpen" style="margin-top: 100px">
+    <div v-if="!isAuthenticated && !isFormOpen" style="margin-top: 100px ; margin-left: 5px ; margin-right: 5px">
       <h3 class="text-lg font-bold mb-4">
-        {{ $t('Please provide your email to start chatting!!!') }}
+        {{ $t('Insert Your Email To Start Chat') }}
       </h3>
       <div>
+        <ValidationObserver v-slot="{ handleSubmit }">
         <label class="email-la" for="email">{{ $t('Email Address') }}:</label>
-        <input
-          class="email"
-          type="email"
-          id="email"
-          v-model="email"
-          @input="validateEmail"
-          required
-        />
-        <button class="btn" @click="startChatting">
-          {{ $t('Start Chatting') }}
+        <ValidationProvider
+          name="email"
+          rules="required|email"
+          v-slot="{ errors }"
+          slim
+        >
+          <input
+            name="email"
+            v-model="email"
+            type="email"
+            :label="$t('Email')"
+            class="email"
+            required
+            :valid="!errors[0]"
+          />
+          <p id= 'error'>{{errors[0]}}</p>
+        </ValidationProvider>
+        <button class="btn" @click="handleSubmit(startChatting)">
+          {{ $t('Start Chat') }}
         </button>
+        </ValidationObserver>
       </div>
     </div>
     <div v-else-if="isAuthenticated || email" class="relative">
@@ -38,11 +50,12 @@
         <div class="mx-3" v-for="message in messages" :key="message.id">
           <div
             v-if="!message.isFromAdmin"
-            class="bg-[#ecfeff] min-h-[50px] w-[75%] ml-[24%] my-2 rounded-lg"
+            class="bg-[#ecfeff] w-[75%] ml-[24%] my-2 rounded-lg"
           >
-            <span class="text-right text-[#000000] justify-center mt-1">{{
-              message.content
-            }}</span>
+            <span
+              class="inline-block text-right text-[#000000] justify-center mt-1 max-w-full break-words"
+              >{{ message.content }}</span
+            >
           </div>
           <div v-else class="bg-[#f4f6ff] min-h-[50px] my-2 w-[75%] rounded-lg">
             <SfIcon
@@ -53,33 +66,26 @@
               :coverage="1"
               class="float-left"
             />
-            <span class="text-left text-[#000000] justify-center mt-1">{{
-              message.content
-            }}</span>
+            <span
+              class="inline-block text-left text-[#000000] justify-center mt-1 max-w-full break-words"
+              >{{ message.content }}</span
+            >
           </div>
-          <!-- <div :class="message.isFromAdmin?'text-left':'text-right'" class="bg-[#ecfeff] min-h-[50px] w-[75%] ml-[24%] my-2 rounded-lg">{{message.content}}</div> -->
         </div>
       </div>
-
-                    <!-- <p>dfhdiusfhsd</p> -->
-                    <!-- <img
-                    src="/spinner.svg"
-                    alt="logo"
-                    height="40px"
-                    width="40px"
-                    class="bg-white mx-4 my-2 rounded-full"
-                    v-show="elementVisible"
-                  /> -->
       <form
         v-if="isFormOpen || isAuthenticated"
         @submit.prevent="sendMessageToAdmin"
-        class="flex items-center fixed invisible lg:visible min-w-full bottom-[60px]"
+        class="flex items-center fixed invisible lg:visible min-w-full bottom-[70px]"
       >
-        <input
+        <textarea
           type="text"
           v-model="messageToSend"
           placeholder="write a message"
-          class="min-w-[17.5rem] h-10 px-4 rounded shadow-md mr-2"
+          rows="2"
+          class="min-w-[17.5rem] max-h-[90%] h-30 px-2 rounded shadow-md no-scrollbar resize-none"
+        
+          @keydown.enter.prevent="handleEnterKey"
         />
         <button class="sendButton" type="submit">
           <!-- <img class="w-[14%] overflow-hidden" src="sendr.png" alt="send icon" /> -->
@@ -104,9 +110,10 @@
 
 <script>
 import { useUiState } from '~/composables';
-import { ref, inject } from '@vue/composition-api';
+import { ref, inject, provide } from '@vue/composition-api';
 import { onSSR } from '@vue-storefront/core';
 import { useUser } from '@vue-storefront/vendure';
+import { ValidationProvider, ValidationObserver, extend } from 'vee-validate';
 import {
   SfInput,
   SfBadge,
@@ -123,11 +130,24 @@ import {
   SfSidebar,
 } from '@storefront-ui/vue';
 import { onMounted } from 'vue';
+import { required, email } from 'vee-validate/dist/rules';
+
+extend('required', {
+  ...required,
+  message: 'This field is required',
+});
+extend('email', {
+  ...email,
+  message: 'Invalid email',
+});
 export default {
   name: 'messageSideBar',
   props: ['messages'],
   components: {
     SfIcon,
+    ValidationProvider,
+    ValidationObserver,
+    SfInput,
   },
   data() {
     return {
@@ -136,11 +156,10 @@ export default {
       elementVisible: false,
       isFormOpen: false,
       email: '',
+
+
     };
   },
-  // mounted() {
-  //   this.scrollToBottom();
-  // },
   created() {},
   mounted() {
     this.$nextTick(() => {
@@ -151,15 +170,22 @@ export default {
   updated() {
     this.scrollToBottom();
   },
-  methods: {
-    sendMessageToAdmin() {
+    methods: {
+      handleEnterKey(event) {
+    if (event.shiftKey) {
+      this.messageToSend += '\n';
+    } else {
+      this.sendMessageToAdmin();
+    }
+  },
+     sendMessageToAdmin() {
       setTimeout(() => (this.elementVisible = true), 100);
 
       setTimeout(() => (this.elementVisible = false), 5000);
       this.$emit('sendMessageToAdmin', this.messageToSend);
       this.messageToSend = '';
     },
-      scrollToBottom() {
+    scrollToBottom() {
       this.$nextTick(() => {
         const chatContainer = this.$refs.chatContainer;
         chatContainer.scrollTo({
@@ -167,37 +193,25 @@ export default {
           behavior: 'smooth',
         });
       });
-      },
-    validateEmail() {
-      const emailPattern = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
-
-      if (this.email.match(emailPattern)) {
-        this.emailError = '';
-      } else {
-        this.emailError = 'Please enter a valid email address.';
-      }
     },
-    startChatting() {
-      if (this.emailError) {
-        console.log('Email is invalid. Please correct it.');
-      } else {
+    startChatting() 
+    {
         this.$emit('submitt', this.email);
         this.isFormOpen = true;
-        console.log('Email is valid. Starting chat...');
         this.scrollToBottom();
-      }
     },
     scrollToBottom() {
-        this.$nextTick(() => {
+      this.$nextTick(() => {
         const chatContainer = this.$refs.chatContainer;
         chatContainer.scrollTop = chatContainer.scrollHeight;
       });
     },
   },
-  
+
   setup() {
     const { isMessageSideBarOpen } = useUiState();
     const email = ref('');
+    const errorMessage = ref('');
     const { isAuthenticated, load: loadUser, user } = useUser();
     (async () => await loadUser())();
     return {
@@ -205,9 +219,9 @@ export default {
       email,
       isAuthenticated,
       user,
-      email,
+      errorMessage,
     };
-  }
+  },
 };
 </script>
 
@@ -228,10 +242,14 @@ export default {
   font-size: 1.25rem;
   font-weight: bold;
   margin-bottom: 1rem;
+  font-family: 'Montserrat', sans-serif;
+  color: #3860a7;
 }
 .email-la {
   font-weight: bold;
   margin-block-start: 100px;
+  font-family: 'Montserrat', sans-serif;
+  color: #3860a7;
 }
 
 .email {
@@ -256,5 +274,10 @@ export default {
 }
 .chat {
   overflow-y: hidden;
+}
+#error {
+  color: red;
+  font-size: 12px;
+  margin: 2px;
 }
 </style>
